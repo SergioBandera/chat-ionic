cbc12121133fbb45b5d60449b8404c8cd8e4872a
sergio <sergio_bandera@hotmail.com>
2022-05-27 14:58:23 +0200

    foto error,async

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { cameraOutline, send, documentAttachOutline } from "ionicons/icons";
 import {
   IonButton,
+  IonCol,
   IonContent,
   IonFooter,
   IonHeader,
   IonIcon,
+  IonImg,
   IonList,
   IonPage,
   IonTextarea,
@@ -13,44 +15,74 @@ import {
   TextareaChangeEventDetail,
 } from "@ionic/react";
 import "./Chat.css";
+import { UserPhoto } from "../hooks/usePhotoGalery";
+import { usePhotoGallery } from "../hooks/usePhotoGalery";
 
 interface datosMensaje {
   mensaje: string;
+  UserPhoto: UserPhoto;
   hora: string;
   dia: string;
 }
+
 const Chat: React.FC = () => {
+  //useStates
   const [mensajes, setMensajes] = useState<Array<datosMensaje>>();
-  const [mensajeTexto, setMensajeTexto] = useState<datosMensaje>({
-    mensaje: "",
-    hora: "",
-    dia: "",
-  });
-  
+  const [mensajeTexto, setMensajeTexto] = useState<datosMensaje>();
+  const { photos, takePhoto } = usePhotoGallery();
+
+  useEffect(() => {
+    console.log(photos)
+  }, [mensajeTexto]);
+
+  const subirFoto = async () => {
+    await takePhoto();
+    const data = Date().toLocaleString();
+    const fecha = data.split(" ");
+    setMensajeTexto({
+      mensaje: "",
+      UserPhoto: {
+        filepath: "",
+        webviewPath: photos?.webviewPath,
+      },
+      hora: fecha[4],
+      dia: fecha[0],
+    });
+  };
 
   const cogerMensaje = (event: CustomEvent<TextareaChangeEventDetail>) => {
+    //coger dia y hora
     const data = Date().toLocaleString();
     const fecha = data.split(" ");
 
     setMensajeTexto({
       mensaje: event.detail.value!,
+      UserPhoto: {
+        filepath: "",
+        webviewPath: "",
+      },
       hora: fecha[4],
       dia: fecha[0],
     });
   };
 
   const enviarMensaje = () => {
-    if (mensajes != null) setMensajes([...mensajes!, mensajeTexto]);
-    else setMensajes([mensajeTexto]);
-    
+    if (mensajes != null) setMensajes([...mensajes!, mensajeTexto!]);
+    else setMensajes([mensajeTexto!]);
   };
 
+  //pintar todos los mensajes
   const mostrarMensajes = () => {
     if (mensajes != null)
-      return mensajes.map(({ mensaje, hora, dia }, index) => (
+      return mensajes.map(({ mensaje, UserPhoto, hora, dia }, index) => (
         <IonList className="caja-mensaje" key={index}>
-          <p className="fecha">{dia} {hora}</p>
+          <p className="fecha">
+            {dia} {hora}
+          </p>
           <p className="mensaje">{mensaje}</p>
+          {UserPhoto.webviewPath && (
+            <IonImg src={UserPhoto.webviewPath}></IonImg>
+          )}
         </IonList>
       ));
   };
@@ -60,7 +92,7 @@ const Chat: React.FC = () => {
       <IonHeader>
         <IonTitle>Chat con Ionic</IonTitle>
       </IonHeader>
-      <IonContent >
+      <IonContent>
         <IonHeader collapse="condense">
           <IonTitle size="large">Chat</IonTitle>
         </IonHeader>
@@ -69,7 +101,7 @@ const Chat: React.FC = () => {
       <IonFooter>
         <div className="caja-escribir">
           <IonTextarea
-          clearOnEdit
+            clearOnEdit
             className="input-chat"
             name="texto"
             inputMode="text"
@@ -82,11 +114,14 @@ const Chat: React.FC = () => {
             <IonButton className="icono-documento" fill="outline">
               <IonIcon icon={documentAttachOutline} size="medium"></IonIcon>
             </IonButton>
-            <IonButton className="icono-camara" fill="outline">
+            <IonButton
+              onClick={subirFoto}
+              className="icono-camara"
+              fill="outline"
+            >
               <IonIcon icon={cameraOutline} size="medium"></IonIcon>
             </IonButton>
             <IonButton
-            
               onClick={enviarMensaje}
               className="icono-enviar"
               fill="outline"
